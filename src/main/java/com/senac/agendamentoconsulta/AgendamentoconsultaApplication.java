a2fce48b95e142f8b7db317dc1c3191c15f50871
Kaua-0703 <kauagregorio7@gmail.com>
2025-11-13 15:20:15 -0300

    Criando CI/CD

--- src/main/java/com/senac/agendamentoconsulta/AgendamentoconsultaApplication.java
+++ src/main/java/com/senac/agendamentoconsulta/AgendamentoconsultaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AgendamentoconsultaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AgendamentoconsultaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AgendamentoconsultaApplication.class, args);
+    }
 
 }
